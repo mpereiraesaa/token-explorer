5e1dd12c5334a6b35a380886b5a0f57d0eca4953
Manuel Pereira <manuelpereiralds@gmail.com>
2024-05-18 15:49:49 +0200

    feat: Add onboarding store usage in Login component

--- frontend/src/app/components/Navbar/Login.tsx
+++ frontend/src/app/components/Navbar/Login.tsx
@@ -5,9 +5,11 @@ import { useOnboarding } from '@/app/hooks/useOnboarding';
 import { useAccount, useChainId, useSignMessage } from 'wagmi';
 import { Chain, WELCOME_MESSAGE } from '@/constants/constants';
 import { useErrorStore } from '@/app/stores/error';
+import { useOnboardingStore } from '@/app/stores/onboarding';
 
 export const LoginButton = () => {
   const [error, isPending, data, mutateAsync] = useOnboarding();
+  const { setAuthentication } = useOnboardingStore((state) => state);
   const { showNotification } = useErrorStore((state) => state);
   const { address } = useAccount();
   const chainId = useChainId();
@@ -17,7 +19,11 @@ export const LoginButton = () => {
     if (error) {
       showNotification('Error', error.message, 'error');
     }
-  }, [error, isPending, data, showNotification]);
+    if (data) {
+      setAuthentication(true);
+      showNotification('Success', 'Login successful', 'success');
+    }
+  }, [error, isPending, data, showNotification, setAuthentication]);
 
   const handleClick = async () => {
     const signature = await signMessageAsync({ message: WELCOME_MESSAGE });
